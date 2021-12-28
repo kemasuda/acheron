e76b12519d26b234dc96fdb51b32a2752367195b
Kento Masuda <k_masuda@dhcp184.ess.sci.osaka-u.ac.jp>
2021-12-28 11:30:57 +0900

    prepare input files for isochrone fitting

---
+++ input/prepare_isoinputs.py
@@ -0,0 +1,18 @@
+#%%
+import numpy as np
+import pandas as pd
+from jaxstar.utils import correct_gedr3_parallax, correct_kmag
+
+#%%
+filename = "cks/all_edr3_binflag.csv"
+d = pd.read_csv(filename)
+d = correct_gedr3_parallax(d)
+d = correct_kmag(d)
+d.to_csv("isoinput_cks.csv", index=False)
+
+#%%
+filename = "hall/all_edr3_binflag.csv"
+d = pd.read_csv(filename)
+d = correct_gedr3_parallax(d)
+d = correct_kmag(d)
+d.to_csv("isoinput_hall.csv", index=False)
